3ec31c58c5192184812f8aa503b6c0d37d9656a9
stephcole <stephcole989@gmail.com>
2023-07-06 20:56:51 -0400

    JS Project 2

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,8 @@
+
+//Button function//
+function Make_This_Function_Work() {
+    var str = "PUSH THE BUTTON!"
+    + " DON'T PUSH THE BUTTON!"
+    + " AHAHAHAHA!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
