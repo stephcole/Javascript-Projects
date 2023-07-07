6554c5c386707d4b26b05eefadb2454f2fb710a9
stephcole <stephcole989@gmail.com>
2023-07-06 20:07:53 -0400

    Javascript_project_1

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+window.alert('Welcome to the Dark Side of the MOON!');
+
+document.write('Enjoy, party people!');
+
+
+//here is am combining two variables//
+var Sent1="This is the beginning of the string,"
+var Sent2=" and this is the end of the string."
+document.write(Sent1
++Sent2);
+
+
+
+//inserting expression//
+
+Document.write(12 + 12);
